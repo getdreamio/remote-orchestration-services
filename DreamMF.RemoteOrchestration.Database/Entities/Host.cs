5f2a07a2c0721376397cbfcfce212a861207d813
Chris Fryer <cfryerdev@gmail.com>
2024-12-03 13:32:26 -0700

    Added environment

--- DreamMF.RemoteOrchestration.Database/Entities/Host.cs
+++ DreamMF.RemoteOrchestration.Database/Entities/Host.cs
@@ -7,6 +7,7 @@ public class Host
     public string Name { get; set; } = string.Empty;
     public string Url { get; set; } = string.Empty;
     public string Key { get; set; } = string.Empty;
+    public string Environment { get; set; } = string.Empty;
     public DateTime Created_Date { get; set; }
     public DateTime Updated_Date { get; set; }
 }
